52090811f4040dc950d1ec536decf265c223320c
박지은 <148677085+www-author@users.noreply.github.com>
2024-01-30 16:46:12 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,17 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int sum = 0;
-        int index = 0;
+        int sum = 0; 
         
-        while(index < n) {
-            index++;
-            if (index % 2 !=0){
+        for(int i = 0; i <= n ;i++) {
+            if (i % 2 != 0) {
                 continue;
             }
-            sum += index;
+            sum += i;
         }
-        
-        
         return sum;
     }
 }
