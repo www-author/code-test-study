39deeaef3446357a4c4c0c1ded2087d208ab341d
박지은 <148677085+www-author@users.noreply.github.com>
2024-01-30 17:40:06 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;    
-        for (int i = 0; i < n; i++) {
-            answer += 12000;
-        }
-        for (int i = 0; i< k-n/10; i++) {
-            answer += 2000;
-        }
-        return answer;
-
+        return n * 12000 + (k * 2000 - n/10 * 2000);
     }
 }
