75cda7bc047164fcb2e68fbd9a6d32b0254fc8c2
박지은 <148677085+www-author@users.noreply.github.com>
2024-02-07 17:44:32 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for(int num : num_list) {
+            if (num != n) {
+                continue;
+            }
+            return 1;
+        }
+        return 0;
+    }
+}
