d736480e64b9cf8624e6511f26240bacee04c053
박지은 <148677085+www-author@users.noreply.github.com>
2024-06-13 00:18:16 +0900

    [level 2] Title: 가장 큰 수, Time: 2.44 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+                StringBuilder stringBuilder = new StringBuilder();
+        String[] stringOfNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            stringOfNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(stringOfNumbers, (x, y) -> (y + x).compareTo(x + y));
+
+        if(stringOfNumbers[0].equals("0")) return "0";
+
+        
+        for (String number :stringOfNumbers) {
+            stringBuilder.append(number);
+        }
+        return stringBuilder.toString();
+    }
+}
