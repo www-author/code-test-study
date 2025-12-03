712c3d46f993ad943ff962204efd1614f245d560
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-03 19:34:54 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String[] s : clothes) {
+            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
+        }
+        
+        for (String s : map.keySet()) {
+            answer *= (map.get(s) + 1);
+        }
+        answer--;
+        return answer;
+    }
+}
