a57d2ea405bb324b6a6e24705871e13070563e6b
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-05 23:39:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.99 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    static boolean visit[][];
+    static int dx[]={0,0,1,-1};
+    static int dy[]={1,-1,0,0};
+    public int solution(int[][] maps) {
+        visit = new boolean[maps.length][maps[0].length];
+        return bfs(0,0,maps);
+    }
+
+    static int bfs(int x, int y, int[][] arr){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x,y,1});
+        visit[x][y]=true;
+        
+        while(!q.isEmpty()){
+            int p[] = q.poll();
+            if (p[0] == arr.length - 1 && p[1] == arr[0].length - 1) {
+                return p[2]; 
+            }
+            for(int i=0;i<4;i++){
+                int nx = p[0] + dx[i];
+                int ny = p[1] + dy[i];
+                if(0<=nx&&nx<arr.length && 0<=ny&&ny<arr[0].length){
+                    if(!visit[nx][ny]&&arr[nx][ny]==1){
+                        visit[nx][ny] = true;
+                        q.add(new int[]{nx,ny,p[2]+1});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
