0e677618219ad50f4de39ad27315676eef7edde4
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-11 01:12:30 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 13.23 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int size = sequence.length;
+        
+        for(right = 0; right<sequence.length; right++){
+            sum += sequence[right];
+            while(right<sequence.length && sum > k){
+                sum -=sequence[left];
+                left++;
+            }
+            if (sum == k){
+                if (size > right - left){
+                    size = right - left;
+                    answer[0] = left;
+                    answer[1] = right;
+                }
+            }   
+        }
+        return answer;
+    }
+}
