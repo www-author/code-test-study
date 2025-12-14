9b95c0367bfb54f2310b4ed722c4ab778bf395df
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-15 00:53:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        List<String> list = new ArrayList<>();
+        list.add(words[0]);
+
+        for(int i=1; i<words.length; i++){
+            if(list.contains(words[i]) || same(list.get(list.size()-1), words[i])){
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
+                break;
+            }
+            list.add(words[i]);
+        }
+
+        return answer;
+    }
+
+    public boolean same(String word1, String word2) {
+        if(word1.charAt(word1.length()-1) == word2.charAt(0)){
+            return false;
+        }
+        return true;
+    }
+}
