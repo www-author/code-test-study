715c85c84a69438ff4c7406b9a7941ac313d201b
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-17 00:28:23 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                answer++; 
+            }
+            n /= 2;
+        }
+        return answer;
+    }
+}
