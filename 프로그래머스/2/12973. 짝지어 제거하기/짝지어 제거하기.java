d25c11d42a1b43a20d9bce02c597d16e9ca7e2e2
www-author <dream970215@naver.com>
2025-12-20 01:27:29 +0900

    feat : stack

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,18 +1,13 @@
 import java.util.Stack;
 
-class Solution
-{
- public int solution(String s)
-    {
+class Solution {
+    public int solution(String s) {
         Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray())
-        {
-            if (stack.isEmpty())
-            {
+
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
                 stack.add(c);
-            }
-            else {
+            } else {
                 if (c == stack.peek()) {
                     stack.pop();
                 } else {
