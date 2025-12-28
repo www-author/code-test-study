07c165be95994303e54c993babe85bcf8cd8cedc
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2025-12-29 01:04:43 +0900

    [level 2] Title: 피보나치 수, Time: 6.56 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    int[] data = new int[100001];
+
+    public int solution(int n) {
+        return fibo(n);
+    }
+
+    private int fibo(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (data[n] != 0) {
+            return data[n] % 1234567;
+        }
+
+        return data[n] = (fibo(n - 2) + fibo(n - 1)) % 1234567;
+    }
+}
