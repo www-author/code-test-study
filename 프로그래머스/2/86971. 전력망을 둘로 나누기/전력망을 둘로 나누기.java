dccfbc53f16a616b82bbc78e345001fce16f7034
Park Ji Eun <148677085+www-author@users.noreply.github.com>
2026-01-03 03:08:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.22 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    static private int[] unf;
+    public static int Find(int v){
+        if(v==unf[v]) return v;
+        else return unf[v]=Find(unf[v]);
+    }
+    public static void Union(int a, int b){
+        int fa=Find(a);
+        int fb=Find(b);
+        if(fa!=fb) {
+            if(fa>fb) unf[fa] = fb;
+            else unf[fb] = fa;
+    	}
+	}
+    public static int getRemainder(int n) {
+    	int lt=0, rt=0;
+    	for(int i=1; i<=n; i++) 
+    		if(Find(unf[i])==1) lt++;
+    	rt = n-lt;
+    	return Math.abs(lt-rt);
+    }
+        
+    public int solution(int n, int[][] wires) {
+        int answer = n;
+        for(int i=0;i<wires.length;i++){
+            unf = new int[n+1];
+            for(int p=0;p<=n;p++)unf[p]=p;
+            for(int j=0; j<wires.length; j++) {
+        		if(i==j) continue;
+                
+                Union(wires[j][0],wires[j][1]);
+            }
+            int tmp = getRemainder(n);
+            answer = Math.min(answer,tmp);
+        }
+        
+        return answer;
+    }
+}
